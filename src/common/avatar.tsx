4fc7e6bcac2d71a0a5ecb1c5ec0a1fd3f827b128
Liran Ramekar <liranramekar7@gmail.com>
2025-03-13 19:16:25 +0530

    added Avatar component

--- src/common/avatar.tsx
+++ src/common/avatar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export const Avatar = () => {
-  return (
-    <div className='w-7 h-7 rounded-full bg-white border'>
+type Props = {
+  classes : string
+  children? : React.ReactNode
+}
 
+export const Avatar = ({classes, children} : Props) => {
+  const AvatarClasses = `${classes} rounded-full bg-gradient-to-r from-purple-500 to-red-500 flex justify-center items-center`
+  return (
+    <div className={AvatarClasses}>
+      {children}
     </div>
   )
 }
