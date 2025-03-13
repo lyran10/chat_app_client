ff838c8f6717eb60eb9a1ff654d324b86e984cd8
Liran Ramekar <liranramekar7@gmail.com>
2025-03-13 18:59:28 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
+import { MainContainer } from './common/mainContainer'
+import { Dashboard } from './pages/dashboard'
 
 export const App = () => {
 
   return(
-    // <>
-    //   <Routes>
-    //     <Route path='/' element={<MainContainer><Dashboard /></MainContainer>} />
-    //   </Routes>
-    // </>
-    <div className='h-screen flex justify-center items-center'>
-        <div className='text-2xl shadow-3xl text-green-500 animate-wiggle flex justify-center items-center h-[300px] w-[300px]'>
-          App
-        </div>
-      </div>
+    <>
+      <Routes>
+        <Route path='/' element={<MainContainer><Dashboard /></MainContainer>} />
+      </Routes>
+    </>
   )
 }
