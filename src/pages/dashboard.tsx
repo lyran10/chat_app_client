6d3dde60a2ac390a369a709efcd265bde5aa1f17
Liran Ramekar <liranramekar7@gmail.com>
2025-03-13 18:45:39 +0530

    pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Sidebar } from '../dashboard/sidebar/sidebar'
+
+export const Dashboard = () => {
+  return (
+    <main className='w-[90%] h-[90%]'>
+      <Sidebar/>
+    </main>
+  )
+}
